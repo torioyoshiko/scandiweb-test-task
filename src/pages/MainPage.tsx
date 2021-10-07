018757781948576adf21146693a2dac1bc6d4f95
Torio Yoshiko <torio.yoshiko@gmail.com>
2021-10-07 13:52:07 +0200

    Fixed overlay in MainPage Added typescript to dependencies

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -50,7 +50,11 @@ const ProductList = styled.div`
 
 const Container = styled.div`
   position: relative;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
   width: 100%;
+  height: 100%;
 `;
 
 const Overlay = styled.div`
